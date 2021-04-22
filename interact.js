d9d89ac754e0ba57875e592aa673a3f8799b38e7
luboslavBurdenski <luboslav990226@gmail.com>
2021-04-22 14:39:10 +0300

    task 5 update

--- interact.js
+++ interact.js
@@ -6,7 +6,7 @@ const run = async function () {
     const wallet = new ethers.Wallet("1ea6a0ac245df86cc26df6d43b333db6e06315235bbf011a1f38a5709e102214", provider)
     const bookLibraryContract = new ethers.Contract("0x5818871A858f71cdBA3Af1B8ACf841fFc0eFf16d", BookLibrary.abi, wallet);
 
-    //await bookLibraryContract.addBook('book2', 10);
+    await bookLibraryContract.addBook('book2', 10);
 
     const booksLength = await bookLibraryContract.getAllBooks();
 
@@ -16,28 +16,26 @@ const run = async function () {
         for (let i = 0; i < booksLength; i++) {
             const bookHash = await bookLibraryContract.bookIds(i);
             const [book, copies] = await bookLibraryContract.books(bookHash);
-            console.log(bookHash)
+            console.log(`Available books: ${book} - ${copies}`);
             arrOfBooks[bookHash.toString()] = [book, Number(copies.toString())];
         }
     }
 
     const entries = Object.entries(arrOfBooks);
-    for (let i = 0; i < entries.length; i++) {
-        console.log(`Available books: ${entries[i][1][0]} - ${entries[i][1][1]}`)
-    }
 
     const firstHashOfBookToWorkWith = entries[0][0].toString();
 
     const borrowingBook = await (await bookLibraryContract.borrowBook(firstHashOfBookToWorkWith)).wait();
+
     const returningBook = await (await bookLibraryContract.returnBook(firstHashOfBookToWorkWith)).wait();
-   
+
     const isRented = await bookLibraryContract.checkIfBookIsRented(firstHashOfBookToWorkWith);
+
     console.log(`Rented: ${isRented}`)
 
     const avalabilityOfBook = await bookLibraryContract.books(firstHashOfBookToWorkWith);
-    entries[0][0] = avalabilityOfBook; //just to update the object, although it is pointless, just added a bit more abstraction :)
-    console.log(`Availability of the book: ${avalabilityOfBook[1]}`)
 
+    console.log(`Availability of the book: ${avalabilityOfBook[1]}`)
 }
 run()
 //etherlime deploy --file=./deployment/deploy.js --network=ropsten --secret=1ea6a0ac245df86cc26df6d43b333db6e06315235bbf011a1f38a5709e102214`
