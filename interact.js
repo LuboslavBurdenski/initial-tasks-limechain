594bc76f2e27c9ce9b8203321ee6fb5bd8839b95
LuboslavBurdenski <75112386+LuboslavBurdenski@users.noreply.github.com>
2021-04-22 16:29:49 +0300

    Revert "task 5 update"

--- interact.js
+++ interact.js
@@ -6,15 +6,13 @@ const run = async function () {
     const wallet = new ethers.Wallet("1ea6a0ac245df86cc26df6d43b333db6e06315235bbf011a1f38a5709e102214", provider)
     const bookLibraryContract = new ethers.Contract("0x5818871A858f71cdBA3Af1B8ACf841fFc0eFf16d", BookLibrary.abi, wallet);
 
-    //adding new book
     await bookLibraryContract.addBook('book2', 10);
-    
+
     const booksLength = await bookLibraryContract.getAllBooks();
-    
+
     let arrOfBooks = {};
 
     if (booksLength > 0) {
-        // making an object with property array in which we save the name and number of copies
         for (let i = 0; i < booksLength; i++) {
             const bookHash = await bookLibraryContract.bookIds(i);
             const [book, copies] = await bookLibraryContract.books(bookHash);
@@ -22,21 +20,21 @@ const run = async function () {
             arrOfBooks[bookHash.toString()] = [book, Number(copies.toString())];
         }
     }
-    //getting the keys and values of our main object
+
     const entries = Object.entries(arrOfBooks);
-    //getting the hash of the first book 
+
     const firstHashOfBookToWorkWith = entries[0][0].toString();
-    //check if book is rented/borrowed
-    const isRented = await bookLibraryContract.checkIfBookIsRented(firstHashOfBookToWorkWith);
-    //borrowing the book
-    console.log(`Rented: ${isRented}`)
+
     const borrowingBook = await (await bookLibraryContract.borrowBook(firstHashOfBookToWorkWith)).wait();
-    //returning the book and checking if the function had executed in the right way
-    console.log(`Rented: ${isRented}`)
+
     const returningBook = await (await bookLibraryContract.returnBook(firstHashOfBookToWorkWith)).wait();
+
+    const isRented = await bookLibraryContract.checkIfBookIsRented(firstHashOfBookToWorkWith);
+
     console.log(`Rented: ${isRented}`)
-    //checking the avalability of the book again
+
     const avalabilityOfBook = await bookLibraryContract.books(firstHashOfBookToWorkWith);
+
     console.log(`Availability of the book: ${avalabilityOfBook[1]}`)
 }
 run()
