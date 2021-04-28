b00b2fba1cbff55728ae855833b9c9d13f761626
luboslavBurdenski <luboslav990226@gmail.com>
2021-04-28 17:59:10 +0300

    work on erc20 task

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -1,18 +1,34 @@
-//const deploy = async (network, secret, etherscanApiKey) => {
+// const deploy = async (network, secret, etherscanApiKey) => {
 
-//	const deployer = new etherlime.EtherlimeGanacheDeployer();
+// 	const deployer = new etherlime.EtherlimeGanacheDeployer();
 // await deployer.deploy(BookLibrary);
-//};
-//contract address; 0x8f97C5A3FC849234C2b5fdB9fa81D26F7A565298
-//tx hash: 0xd708f88e3ffc347d66e985cdf85b75c2e77e78ab57f94987ab278d92c3ab39d0
+// };
+// contract address; 0x8f97C5A3FC849234C2b5fdB9fa81D26F7A565298
+// tx hash: 0xd708f88e3ffc347d66e985cdf85b75c2e77e78ab57f94987ab278d92c3ab39d0
 
 const etherlime = require('etherlime-lib');
-const BookLibrary = require('../build/BookLibrary.json');
+const LIBRARY = require('../build/LIBWrapper.json');
 const deploy = async (network, secret) => {
 	const deployer = new etherlime.InfuraPrivateKeyDeployer(secret, network, '40c2813049e44ec79cb4d7e0d18de173');
-	const result = await deployer.deploy(BookLibrary);
+	const result = await deployer.deploy(LIBRARY);
 	// const deployer = new etherlime.EtherlimeGanacheDeployer();
 	// const result = await deployer.deploy(BookLibrary);
 
 }
 module.exports = { deploy }
+
+
+// const etherlime = require('etherlime-lib');
+// const LIBRARY = require('../build/LIBRARY.json');
+
+
+// const deploy = async (network, secret, etherscanApiKey) => {
+
+// 	const deployer = new etherlime.EtherlimeGanacheDeployer();
+// 	const result = await deployer.deploy(LIBRARY);
+
+// };
+
+// module.exports = {
+// 	deploy
+// };
